7200fd0d79fb3da30005a27294a83fd246a610d9
pinakinmistry <pinakin.nmistry@gmail.com>
2016-02-05 11:28:52 +0400

    Express server with get, sendFile, status, send, json and listen methods

--- server.js
+++ server.js
@@ -2,4 +2,24 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
+var express = require('express');
+var app = express();
+var port = 4000;
+
 var jsonData = {count: 12, message: 'hey'};
+
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html', function (err) {
+		if(err) {
+			res.status(500).send(err);
+		}
+	});
+});
+
+app.get('/data', function (req, res) {
+	res.json(jsonData);
+});
+
+app.listen(port, function () {
+	console.log('listening on http:localhost:' + port);
+});
