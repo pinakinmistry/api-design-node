31a1ebdde78d8fc53cec3e54f2595d8d482ba7df
pinakinmistry <pinakin.nmistry@gmail.com>
2016-03-09 09:00:24 +0530

    Adding tigerRouter

--- server/server.js
+++ server/server.js
@@ -9,20 +9,21 @@ var morgan = require('morgan');
 var lionRouter = require('./lions');
 var tigerRouter = require('./tigers');
 
-app.use(morgan('dev'))
+app.use(morgan('combined'))
 app.use(express.static('client'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 // this is called mounting. when ever a req comes in for
 // '/lion' we want to use this router
 app.use('/lions', lionRouter);
+app.use('/tigers', tigerRouter);
 
 app.use(function(err, req, res, next) {
   if (err) {
-    res.status(500).send(error);
+    res.status(500).send(err);
   }
 });
 
 
-app.listen(3000);
-console.log('on port 3000');
+app.listen(4000);
+console.log('on port 4000');
